144bde95c40a16cc378a11b2c1f278286f7608eb
kalva karthik <210303105615@paruluniversity.ac.in>
2024-12-27 16:15:41 +0530

    crated taskApp class

---
+++ TaskListApp.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class TaskListApp {
+    public static void main(String[] args) {
+        ArrayList<String> tasks = new ArrayList<>();
+        Scanner sc= new Scanner(System.in);
+    }
+}
